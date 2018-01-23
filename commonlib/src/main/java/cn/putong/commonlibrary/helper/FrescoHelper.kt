de8db68c94aa0203ee6ec91c3c3e3bbfd8060464
dingputongxi <dingyan1105@163.com>
2018-01-23 11:57:13 +0800

    提交

--- commonlib/src/main/java/cn/putong/commonlibrary/helper/FrescoHelper.kt
+++ commonlib/src/main/java/cn/putong/commonlibrary/helper/FrescoHelper.kt
@@ -1,6 +1,9 @@
 package cn.putong.commonlibrary.helper
 
+import android.content.Context
 import android.net.Uri
+import android.util.DisplayMetrics
+import android.view.WindowManager
 import com.facebook.drawee.backends.pipeline.Fresco
 import com.facebook.drawee.view.SimpleDraweeView
 import com.facebook.imagepipeline.request.ImageRequestBuilder
@@ -18,9 +21,11 @@ object FrescoHelper {
      * 主要用于加载GIF
      */
     fun setAnimatorController(uri: Uri, simpleDraweeView: SimpleDraweeView) {
+
         val request = ImageRequestBuilder.newBuilderWithSource(uri)
                 .setProgressiveRenderingEnabled(true)
                 .build()
+
         simpleDraweeView.controller =
                 Fresco.newDraweeControllerBuilder()
                         .setAutoPlayAnimations(true) // 设置加载图片完成后是否直接进行播放
@@ -37,6 +42,14 @@ object FrescoHelper {
                         .setAutoPlayAnimations(true)
                         .setUri(uri)
                         .build()
+    }
+
+    private fun getScreenWidth(context: Context): Int {
+        val wm = context
+                .getSystemService(Context.WINDOW_SERVICE) as WindowManager
+        val outMetrics = DisplayMetrics()
+        wm.defaultDisplay.getMetrics(outMetrics)
+        return outMetrics.widthPixels
     }
 
 }
--- commonlib/src/main/java/cn/putong/commonlibrary/otto/event/UpdateDataEvent.kt
+++ commonlib/src/main/java/cn/putong/commonlibrary/otto/event/UpdateDataEvent.kt
@@ -4,4 +4,4 @@ package cn.putong.commonlibrary.otto.event
  * 更新首页年数据Event
  * Created by xinyi on 2018/1/23.
  */
-class UpdateDataEvent()
+class UpdateDataEvent
